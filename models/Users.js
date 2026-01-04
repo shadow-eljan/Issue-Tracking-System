43ee8b7063b1760cf1a4f7b870f18c79d7538108
shadow-eljan <eljanrai25@gmail.com>
2026-01-04 18:21:17 +0545

    Hashing and Checking password

--- models/Users.js
+++ models/Users.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const UserSchema = new mongoose.Schema({
     name:{
@@ -31,6 +32,15 @@ const UserSchema = new mongoose.Schema({
     
     
 })
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return;
+  const salt = await bcrypt.genSalt(10); 
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.comparePassword = async function (password){
+    return await bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model("User", UserSchema);
 export default User;
