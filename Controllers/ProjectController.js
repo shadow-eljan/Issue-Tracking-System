8c0c27d93e06dbfaaa1a062d05e412969bf36fd6
shadow-eljan <eljanrai25@gmail.com>
2026-01-15 19:46:13 +0545

    Adding CRUD functions

--- Controllers/ProjectController.js
+++ Controllers/ProjectController.js
@@ -2,23 +2,23 @@ import Project from "../models/Project.js";
 
 export const createProject = async (req, res) => {
   try {
-    const { name, description, key, members } = req.body;
+    const { name, description, key, createdBy, members } = req.body;
 
     if (!name || !key) {
       return res.status(400).json({ message: "Name and key are required" });
     }
 
     
-    const existingProject = await Project.findOne({ key });
+    const existingProject = await Project.findOne({key});
     if (existingProject) {
-      return res.status(409).json({ message: "Project key already exists" });
+      return res.status(409).send({ message: "Project key already exists" });
     }
 
     const project = await Project.create({
     name: name,
     description: description,
     key: key,
-    createdBy: req.user.id,
+    createdBy: createdBy,
     members: members
     });
 
@@ -28,7 +28,38 @@ export const createProject = async (req, res) => {
       project,
     });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server error" });
+    res.status(400).send({ error: error.message })
   }
 };
+
+export const viewProject = async(req, res) => {
+  const {key} = req.params;
+
+  if (!key) {
+    const allProjects = await Project.find();
+    return res.status(200).json(allProjects);
+  }
+
+  const findProject = await Project.findOne({key});
+  if(findProject){
+    res.status(200).send(findProject);
+  }else{
+    res.status(404).send({message: `Project not found.`});
+  }
+};
+
+export const updateProject = async (req, res)=>{
+
+    const updateID = req.params.id;
+    const newProject = req.body;
+
+    const updated = await Project.findByIdAndUpdate(updateID, newProject, {new: true});
+    res.send({message: `Project: ${updated} updated successfully.`});
+};
+
+export const deleteProject = async(req, res) =>{
+
+  const deleteID = req.params.id;
+  const deleted = await Project.findByIdAndDelete(deleteID);
+  res.send({message: `Project: ${deleteID} deleted.`})
+}
