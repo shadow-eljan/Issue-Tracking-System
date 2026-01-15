91352e1e3a627df7c1eef6c7035e031a59978ff3
shadow-eljan <eljanrai25@gmail.com>
2026-01-15 19:46:31 +0545

    Adding Projects

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectDB from "./db/Connection.js";
 import UserRoutes from "./Routes/UserRoutes.js"
+import projectRouter from "./Routes/ProjectRoutes.js";
 
 const app = express();
 app.use(express.json())
@@ -9,3 +10,4 @@ connectDB().then(()=>{
     app.listen(4000, ()=> console.log("Server is up."));
 })
 app.use("/api/auth", UserRoutes);
+app.use("/api/projects", projectRouter);
