952e4f3fdee0d71a0fa3d35679dedea412bae2e3
shadow-eljan <eljanrai25@gmail.com>
2026-01-15 19:46:54 +0545

    Routing projects methods

---
+++ Routes/ProjectRoutes.js
@@ -0,0 +1,13 @@
+import express from   "express";
+import {createProject, viewProject, updateProject, deleteProject} from "../Controllers/ProjectController.js"
+import router from "./UserRoutes.js";
+
+const projectRouter = express.Router();
+
+projectRouter.get("/view/:key",  viewProject);
+projectRouter.get("/view",  viewProject);
+projectRouter.post("/new", createProject);
+projectRouter.put("/update/:id", updateProject);
+projectRouter.delete("/delete/:id", deleteProject);
+
+export default projectRouter;
