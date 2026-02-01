4836c8f7955257b83ea39cda227f404cd3a6974d
shadow-eljan <eljanrai25@gmail.com>
2026-02-01 20:16:39 +0545

    Enhancing methods

--- Controllers/ProjectController.js
+++ Controllers/ProjectController.js
@@ -1,65 +1,30 @@
 import Project from "../models/Project.js";
 
-export const createProject = async (req, res) => {
-  try {
-    const { name, description, key, createdBy, members } = req.body;
-
-    if (!name || !key) {
-      return res.status(400).json({ message: "Name and key are required" });
-    }
-
-    
-    const existingProject = await Project.findOne({key});
-    if (existingProject) {
-      return res.status(409).send({ message: "Project key already exists" });
-    }
-
-    const project = await Project.create({
-    name: name,
-    description: description,
-    key: key,
-    createdBy: createdBy,
-    members: members
-    });
-
-
-    res.status(201).json({
-      message: "Project created successfully",
-      project,
-    });
-  } catch (error) {
-    res.status(400).send({ error: error.message })
-  }
+const addProject = async (req, res) => {
+  const { name, description, key, members } = req.body;
+  const project = await Project.create({
+    name,
+    description,
+    key,
+    members,
+    createdBy: req.user._id,
+  });
+  res.send({ message: "Project created!" });
 };
 
-export const viewProject = async(req, res) => {
-  const {key} = req.params;
-
-  if (!key) {
-    const allProjects = await Project.find();
-    return res.status(200).json(allProjects);
-  }
-
-  const findProject = await Project.findOne({key});
-  if(findProject){
-    res.status(200).send(findProject);
-  }else{
-    res.status(404).send({message: `Project not found.`});
-  }
+const getProjects = async (req, res) => {
+  const projects = await Project.find().populate(
+    "createdBy",
+    "name email -_id",
+  );
+  res.send(projects);
 };
 
-export const updateProject = async (req, res)=>{
-
-    const updateID = req.params.id;
-    const newProject = req.body;
-
-    const updated = await Project.findByIdAndUpdate(updateID, newProject, {new: true});
-    res.send({message: `Project: ${updated} updated successfully.`});
+const getProjectById = async (req, res) => {
+  const id = req.params.id;
+  const project = await Project.findById(id);
+  if (!project) return res.status(404).send({ error: "Project not found!" });
+  res.send(project);
 };
 
-export const deleteProject = async(req, res) =>{
-
-  const deleteID = req.params.id;
-  const deleted = await Project.findByIdAndDelete(deleteID);
-  res.send({message: `Project: ${deleteID} deleted.`})
-}
+export { addProject, getProjects, getProjectById };
