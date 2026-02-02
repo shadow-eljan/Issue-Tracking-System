7368b049220461f6fee26b6f25bfd848b04df8ae
shadow-eljan <eljanrai25@gmail.com>
2026-02-02 19:38:08 +0545

    Routing user

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { login, registerUser } from "../Controllers/UserController.js";
+import { getAllUsers, login, registerUser } from "../Controllers/UserController.js";
 
 const router = express.Router();
 
+router.get("/view", getAllUsers);
 router.post("/register", registerUser);
 router.post("/login", login);
+
 
 export default router;
