3feacf59b31d5eec1c1dcde61f64094562dd1ab6
shadow-eljan <eljanrai25@gmail.com>
2026-02-08 18:51:48 +0545

    app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import connectDB from "./db/Connection.js";
 import UserRoutes from "./Routes/UserRoutes.js"
 import projectRouter from "./Routes/ProjectRoutes.js";
-
+import issueRouter from "./Routes/IssueRoutes.js";
 const app = express();
 app.use(express.json())
 
@@ -11,3 +11,4 @@ connectDB().then(()=>{
 })
 app.use("/api/auth", UserRoutes);
 app.use("/api/projects", projectRouter);
+app.use("/api/issues", issueRouter);
